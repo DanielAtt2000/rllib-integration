997853184036c87bf60fb06a1206bac19511e6dd
Daniel Attard <attarddan2017@gmail.com>
2022-12-09 09:00:09 +0100

    checker updated

--- checker.py
+++ checker.py
@@ -1,10 +1,19 @@
 import random
+
+from git import Repo
+
 def check_with_user():
+    repo = Repo('.')
     verify_num = random.randint(0,100)
     print('Any observations in Rewards were checked?')
     print('Everything committed?')
     print('Hash commit used in name?')
     print('Using the latest version of the experiment?')
+
+    if repo.is_dirty():
+        print('----------> Uncommitted files <-------------')
+        return False
+
     user_input = input(f'Enter the value {verify_num}')
 
     if int(user_input) == verify_num:
---
+++ run.py
@@ -0,0 +1,23 @@
+#/usr/bin/python
+
+from git import Repo
+
+repo = Repo('.')
+if repo.is_dirty():
+    print('----------> Uncommitted files <-------------')
+else:
+    remote = repo.remote('origin')
+    remote.fetch()
+    latest_remote_commit = remote.refs[repo.active_branch.name].commit
+    latest_local_commit = repo.head.commit
+
+    print(f"Latest commit is pushed: {latest_local_commit == latest_remote_commit}")
+    print()
+    print()
+
+    print("conda activate CarlaRlib")
+    print(f"python ppo_train.py ppo_example/ppo_config.yaml --name ppo_{latest_local_commit[:6]}")
+    print(f"python dqn_train.py dqn/dqn_config.yaml --name dqn_{latest_local_commit[:6]}")
+
+
+
