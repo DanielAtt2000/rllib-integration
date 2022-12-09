4401ca9f515f744bdf06c80550c605af3435db76
Daniel Attard <attarddan2017@gmail.com>
2022-12-09 09:30:39 +0100

    small update

--- run.py
+++ run.py
@@ -1,14 +1,19 @@
 #/usr/bin/python
+try:
+    from git import Repo
 
-from git import Repo
+    repo = Repo('.')
+    if repo.is_dirty():
+        print('----------> Uncommitted files <-------------')
+    else:
+        print("conda activate CarlaRlib")
+        print(f"python ppo_train.py ppo_example/ppo_config.yaml --name ppo")
+        print(f"python dqn_train.py dqn/dqn_config.yaml --name dqn")
 
-repo = Repo('.')
-if repo.is_dirty():
-    print('----------> Uncommitted files <-------------')
-else:
+except:
     print("conda activate CarlaRlib")
-    print(f"python ppo_train.py ppo_example/ppo_config.yaml --name ppo")
-    print(f"python dqn_train.py dqn/dqn_config.yaml --name dqn")
+
+
 
 
 
