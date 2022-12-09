44b373f806fff3a4dfe32e6a3841fc5258354a68
Daniel Attard <attarddan2017@gmail.com>
2022-12-09 09:07:30 +0100

    checker updated

--- checker.py
+++ checker.py
@@ -5,16 +5,17 @@ from git import Repo
 def check_with_user():
     repo = Repo('.')
     verify_num = random.randint(0,100)
+    print('=========================================')
+    print(verify_num)
     print('Any observations in Rewards were checked?')
-    print('Everything committed?')
-    print('Hash commit used in name?')
     print('Using the latest version of the experiment?')
+    print('=========================================')
 
     if repo.is_dirty():
         print('----------> Uncommitted files <-------------')
         return False
 
-    user_input = input(f'Enter the value {verify_num}: ')
+    user_input = input(f'Enter the value: ')
 
     if int(user_input) == verify_num:
         return True
