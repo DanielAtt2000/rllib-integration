3f884d3215ec9d16817d80f8595802c3805f3034
Daniel Attard <attarddan2017@gmail.com>
2022-12-25 09:12:16 +0100

    Updated REward system

--- rllib_integration/TestingWayPointUpdater.py
+++ rllib_integration/TestingWayPointUpdater.py
@@ -84,7 +84,7 @@ def plot_points(previous_position, current_position, next_position, current_wayp
     # plt.(range(0,10))
     # plt.yticks(range(0,10))
     plt.xlim([0.45,0.6])
-    plt.ylim([0.6,0.7])
+    plt.ylim([0.64,0.74])
 
     print(f"x_pos {current_position.x} y_pos {current_position.y}")
     print(f"x_last {previous_position.x} y_last {previous_position.y}")
