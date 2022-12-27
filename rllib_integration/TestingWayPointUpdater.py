16d3bfdc84775ce504010359b85f38a29ab1e77c
Daniel Attard <attarddan2017@gmail.com>
2022-12-27 11:33:09 +0100

    Updated REward system

--- rllib_integration/TestingWayPointUpdater.py
+++ rllib_integration/TestingWayPointUpdater.py
@@ -100,7 +100,7 @@ def plot_points(previous_position, current_position, next_position, current_wayp
     plt.plot(next_position.x, next_position.y, marker="o", markersize=3, markeredgecolor="yellow", markerfacecolor="yellow",label='Next Position')
     plt.plot([current_position.x,previous_position.x],[current_position.y,previous_position.y])
     plt.plot([current_position.x,next_position.x],[current_position.y,next_position.y])
-
+    plt.gca().invert_yaxis()
     # val = update_next_waypoint(current_position.x,current_position.y,previous_position.x,previous_position.y,next_position.x,next_position.y)
 
 
