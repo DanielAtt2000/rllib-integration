5d035794c6e20a207493d384c6529be4df7d6e5c
Daniel Attard <attarddan2017@gmail.com>
2023-02-10 11:17:18 +0100

    DQN

--- dqn/dqn_experiment_basic.py
+++ dqn/dqn_experiment_basic.py
@@ -41,7 +41,7 @@ class DQNExperimentBasic(BaseExperiment):
         self.max_lidar_values = -100000
         self.lidar_max_points = self.config["hero"]["lidar_max_points"]
         self.counter = 0
-        self.visualiseRoute = True
+        self.visualiseRoute = False
         self.visualiseImage = False
         self.counterThreshold = 10
         self.last_hyp_distance_to_next_waypoint = 0
