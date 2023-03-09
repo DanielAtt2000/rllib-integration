8f668303d725a5031733e42b7843996bbf2d3c39
Daniel Attard <attarddan2017@gmail.com>
2023-03-09 12:39:18 +0100

    DQN -values only

--- play_area/neural_network_2/train.py
+++ play_area/neural_network_2/train.py
@@ -9,7 +9,7 @@ import pickle
 
 # for creating validation set
 from sklearn.model_selection import train_test_split
-
+import os
 # for evaluating the model
 from sklearn.metrics import accuracy_score
 from tqdm import tqdm
@@ -23,6 +23,7 @@ from torch.optim import Adam, SGD
 
 def read_data_from_pickle(filename):
     with open(filename, 'rb') as handle:
+        print(filename)
         return pickle.load(handle)
 
 def save_data( filename, data):
@@ -37,53 +38,66 @@ def save_data( filename, data):
 #
 # sample_submission = pd.read_csv('sample_submission_I5njJSF.csv')
 
+if not os.path.isfile("lidar_data.pkl"):
 
-data = read_data_from_pickle('../../image_data/collision_data_2.pkl')
+    data = read_data_from_pickle('../../image_data/collision_data_2.pkl')
+    data = data.loc[:10]
+    data['lidar_image'] = data.loc[:, 'filename']
 
-for index, row in data.iterrows():
-    path = f"../../image_data/lidar/{row['filename']}.pkl"
-    row['filename'] = read_data_from_pickle(path)
-    print(index)
 
-save_data('lidar_data.pkl',data)
+    data['lidar_image'] = data['lidar_image'].apply(lambda x: read_data_from_pickle(f"../../image_data/lidar/{x}.pkl") )
+    # for index, row in data.iterrows():
+    #     path = f"../../image_data/lidar/{row['filename']}.pkl"
+    #     row['image'] = read_data_from_pickle(path)
+    #
+    #     print(index)
 
-train.head()
+    print('Saving File ....')
+    save_data('lidar_data.pkl',data)
 
-# loading training images
-train_img = []
-for img_name in tqdm(train['id']):
-    # defining the image path
-    image_path = 'train_LbELtWX/train/' + str(img_name) + '.png'
-    # reading the image
-    img = imread(image_path, as_gray=True)
-    # normalizing the pixel values
-    img /= 255.0
-    # converting the type of pixel to float 32
-    img = img.astype('float32')
-    # appending the image into the list
-    train_img.append(img)
+else:
+    print('Reading data ....')
+    data = read_data_from_pickle(f"lidar_data.pkl")
+    print('Data Read')
+
+data.head()
+
+# # loading training images
+# train_img = []
+# for img_name in tqdm(train['id']):
+#     # defining the image path
+#     image_path = 'train_LbELtWX/train/' + str(img_name) + '.png'
+#     # reading the image
+#     img = imread(image_path, as_gray=True)
+#     # normalizing the pixel values
+#     img /= 255.0
+#     # converting the type of pixel to float 32
+#     img = img.astype('float32')
+#     # appending the image into the list
+#     train_img.append(img)
 
 # converting the list to numpy array
-train_x = np.array(train_img)
+train_x = np.array(data['lidar_image'].values.tolist())
 # defining the target
-train_y = train['label'].values
-train_x.shape
+train_y = data['done_collision'].values
+print(train_x.shape)
 
-# visualizing images
-i = 0
-plt.figure(figsize=(10,10))
-plt.subplot(221), plt.imshow(train_x[i], cmap='gray')
-plt.subplot(222), plt.imshow(train_x[i+25], cmap='gray')
-plt.subplot(223), plt.imshow(train_x[i+50], cmap='gray')
-plt.subplot(224), plt.imshow(train_x[i+75], cmap='gray')
+
+# # visualizing images
+# i = 0
+# plt.figure(figsize=(10,10))
+# plt.subplot(221), plt.imshow(train_x[i], cmap='gray')
+# plt.subplot(222), plt.imshow(train_x[i+25], cmap='gray')
+# plt.subplot(223), plt.imshow(train_x[i+50], cmap='gray')
+# plt.subplot(224), plt.imshow(train_x[i+75], cmap='gray')
 
 # create validation set
-train_x, val_x, train_y, val_y = train_test_split(train_x, train_y, test_size = 0.1)
-(train_x.shape, train_y.shape), (val_x.shape, val_y.shape)
+train_x, test_x, train_y, test_y = train_test_split(train_x, train_y, test_size = 0.3, shuffle=True, stratify=train_y)
+(train_x.shape, train_y.shape), (test_x.shape, test_y.shape)
 
 
 # converting training images into torch format
-train_x = train_x.reshape(54000, 1, 28, 28)
+train_x = train_x.reshape(len(train_x), 1, 240, 320)
 train_x  = torch.from_numpy(train_x)
 
 # converting the target into torch format
@@ -91,19 +105,20 @@ train_y = train_y.astype(int);
 train_y = torch.from_numpy(train_y)
 
 # shape of training data
-train_x.shape, train_y.shape
-
+print(train_x.shape)
+print(train_y.shape)
 
 # converting validation images into torch format
-val_x = val_x.reshape(6000, 1, 28, 28)
-val_x  = torch.from_numpy(val_x)
+test_x = test_x.reshape(len(test_x), 1, 240, 320)
+test_x  = torch.from_numpy(test_x)
 
 # converting the target into torch format
-val_y = val_y.astype(int)
-val_y = torch.from_numpy(val_y)
+test_y = test_y.astype(int)
+test_y = torch.from_numpy(test_y)
 
 # shape of validation data
-val_x.shape, val_y.shape
+print(test_x.shape)
+print(test_y.shape)
 
 from Model.Model import Net
 # defining the model
@@ -113,9 +128,9 @@ optimizer = Adam(model.parameters(), lr=0.07)
 # defining the loss function
 criterion = CrossEntropyLoss()
 # checking if GPU is available
-if torch.cuda.is_available():
-    model = model.cuda()
-    criterion = criterion.cuda()
+model = model.cuda()
+criterion = criterion.cuda()
+
 
 print(model)
 
@@ -125,42 +140,47 @@ def train(epoch):
     tr_loss = 0
     # getting the training set
     x_train, y_train = Variable(train_x), Variable(train_y)
-    # getting the validation set
-    x_val, y_val = Variable(val_x), Variable(val_y)
+    x_train = x_train.cuda()
+
+    y_train = y_train.cuda()
+    y_train = y_train.float()
+    # # getting the validation set
+    # x_val, y_val = Variable(val_x), Variable(val_y)
     # converting the data into GPU format
-    if torch.cuda.is_available():
-        x_train = x_train.cuda()
-        y_train = y_train.cuda()
-        x_val = x_val.cuda()
-        y_val = y_val.cuda()
+
+
+        # x_val = x_val.cuda()
+        # y_val = y_val.cuda()
 
     # clearing the Gradients of the model parameters
     optimizer.zero_grad()
 
     # prediction for training and validation set
     output_train = model(x_train)
-    output_val = model(x_val)
+    # output_val = model(x_val)
 
     # computing the training and validation loss
     loss_train = criterion(output_train, y_train)
-    loss_val = criterion(output_val, y_val)
+    # loss_val = criterion(output_val, y_val)
     train_losses.append(loss_train)
-    val_losses.append(loss_val)
+    # val_losses.append(loss_val)
 
     # computing the updated weights of all the model parameters
     loss_train.backward()
     optimizer.step()
     tr_loss = loss_train.item()
+    print(f"Epoch:{epoch}")
     if epoch % 2 == 0:
         # printing the validation loss
-        print('Epoch : ', epoch + 1, '\t', 'loss :', loss_val)
+        print('Epoch : ', epoch + 1, '\t', 'loss :', tr_loss)
+
 
 # defining the number of epochs
 n_epochs = 25
 # empty list to store training losses
 train_losses = []
-# empty list to store validation losses
-val_losses = []
+# # empty list to store validation losses
+# val_losses = []
 # training the model
 for epoch in range(n_epochs):
     train(epoch)
@@ -168,7 +188,7 @@ for epoch in range(n_epochs):
 
 # plotting the training and validation loss
 plt.plot(train_losses, label='Training loss')
-plt.plot(val_losses, label='Validation loss')
+# plt.plot(val_losses, label='Validation loss')
 plt.legend()
 plt.show()
 
@@ -181,16 +201,36 @@ prob = list(softmax.numpy())
 predictions = np.argmax(prob, axis=1)
 
 # accuracy on training set
-accuracy_score(train_y, predictions)
+print(f"Accuracy Score training set {accuracy_score(train_y, predictions)}")
 
 
-# prediction for validation set
+
+# TESTING SET
+
+
+# generating predictions for test set
 with torch.no_grad():
-    output = model(val_x.cuda())
+    output = model(test_x.cuda())
 
 softmax = torch.exp(output).cpu()
 prob = list(softmax.numpy())
 predictions = np.argmax(prob, axis=1)
 
-# accuracy on validation set
-accuracy_score(val_y, predictions)
+# replacing the label with prediction
+sample_submission['label'] = predictions
+sample_submission.head()
+
+# saving the file
+sample_submission.to_csv('submission.csv', index=False)
+
+
+# # prediction for validation set
+# with torch.no_grad():
+#     output = model(val_x.cuda())
+#
+# softmax = torch.exp(output).cpu()
+# prob = list(softmax.numpy())
+# predictions = np.argmax(prob, axis=1)
+#
+# # accuracy on validation set
+# accuracy_score(val_y, predictions)
