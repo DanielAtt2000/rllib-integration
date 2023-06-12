cee6cec5fb025d6ee3406f41b464981e80518b5e
Daniel Attard <attarddan2017@gmail.com>
2023-06-12 18:58:47 +0200

    Making exactly the same as a0458620

--- sac_train.py
+++ sac_train.py
@@ -122,7 +122,7 @@ def main():
     specific_version = False
     check_commit = True
 
-    save_to_pickle('waiting_times',[0,20,20,20,20])
+    save_to_pickle('waiting_times',[0,20,30,40,50])
 
     if check_with_user(check_commit):
         args.name = args.name + '_' + str(commit_hash())
