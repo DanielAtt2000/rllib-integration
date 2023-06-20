9496eb1e828dbe4b7f7f96df4ac2d413711ec282
Daniel Attard <attarddan2017@gmail.com>
2023-06-21 01:44:14 +0200

    Angle Between waypoints

--- DataAnalysis/NewProcessData2.py
+++ DataAnalysis/NewProcessData2.py
@@ -73,7 +73,7 @@ def min_max_normalisation(name, value):
 
 no_changes = True
 log = False
-data_file = 'data_3ee0eaee3a8'
+data_file = 'data_6baa849bf25'
 directory = '/home/daniel/data-rllib-integration/data/' + data_file
 for_graphs = False
 def main():
