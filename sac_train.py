58ee3e9e6663318f5bf6d4ffaca29fa71e915fb5
Daniel Attard <attarddan2017@gmail.com>
2023-06-23 16:28:38 +0200

    With GREATER distance to center of lane

--- sac_train.py
+++ sac_train.py
@@ -122,7 +122,7 @@ def main():
     specific_version = False
     check_commit = True
 
-    save_to_pickle('waiting_times',[0,20,40,60,80,100,120,140,160,180,0,20,40,60,80,100,120,140,160,180])
+    save_to_pickle('waiting_times',[0,20,40,60,85,105,125,145,165,185,0,20,40,60,80,100,120,140,160,180])
 
     if check_with_user(check_commit):
         args.name = args.name + '_' + str(commit_hash())
