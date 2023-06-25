6a55154cc154e3b7b276016475dbc9da0780dd1c
Daniel Attard <attarddan2017@gmail.com>
2023-06-25 16:16:06 +0200

    making some changes to get inference results

--- inference_results/VisualiseInferenceResults.py
+++ inference_results/VisualiseInferenceResults.py
@@ -1,12 +1,17 @@
+import random
+
 import numpy as np
 import pandas as pd
 
 results = pd.read_csv(
-    'latest/medium/easy/_home_daniel_ray_results_carla_rllib_sac_4c0293c613_CustomSACTrainer_CarlaEnv_b1f1d_00000_0_2023-06-24_10-54-14_checkpoint_027000.csv')
+    'latest/combined/easy/_home_daniel_ray_results_carla_rllib_sac_4c0293c613_CustomSACTrainer_CarlaEnv_b1f1d_00000_0_2023-06-24_10-54-14_checkpoint_027000.csv')
 results = results[results.route != 'route']
 routes = results['route'].unique()
 
-
+print('HAVE YOU ENSURED THAT IF YOU MEREGED TO ANALYSIS TOGETHER THAT THEY DO NOT CONTAIN THE SAME ROUTES?')
+x = random.randint(0,10)
+if x < 5:
+    input('Yes?')
 
 results = results.replace(['False'],0)
 results = results.replace(['True'],1)
@@ -33,6 +38,8 @@ for route in routes:
         x['route'] = route
         x['timesteps'] = x['timesteps']/no_of_runs
         x['distance_to_center_of_lane'] = x['distance_to_center_of_lane']/no_of_runs
+        # if int(x['route'].split('|')[0]) in [35,69,27,4,64]:
+        # if no_of_runs_successful < 15:
         print('----------------')
         print(f'Route {x["route"]} tested {no_of_runs} times')
         print(x)
@@ -44,3 +51,8 @@ print('OVERLALL RESULTS')
 print(f'Total Runs {total_runs}')
 print(f'Success rate overall = {total_success / total_runs}')
 print(f'Unsuccessful rate overall = {total_unsuccessful / total_runs}')
+
+print('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+print('vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv')
+print('HAVE YOU ENSURED THAT IF YOU MEREGED TO ANALYSIS TOGETHER THAT THEY DO NOT CONTAIN THE SAME ROUTES?')
+print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
