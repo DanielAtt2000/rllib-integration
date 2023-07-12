ee351b410350b6f4dabcc807d8b0e2b1cb655fe3
Daniel Attard <attarddan2017@gmail.com>
2023-07-12 21:40:45 +0200

    Back to 5e8b51ab

--- sac/sac_experiment_basic.py
+++ sac/sac_experiment_basic.py
@@ -404,8 +404,8 @@ class SACExperimentBasic(BaseExperiment):
         """
         obs_space = Dict( {
             'values':Box(
-                low=np.array([0,0,0,0,0,0,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),
-                high=np.array([100,200,200,200,200,20,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]),
+                low=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,-math.pi,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),
+                high=np.array([100,200,200,200,200,20,1,1,1,1,1,1,1,1,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,math.pi,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]),
                 dtype=np.float32
             )
         })
@@ -685,48 +685,48 @@ class SACExperimentBasic(BaseExperiment):
                 previous_position=core.route[core.last_waypoint_index].location,
                 current_position=core.route[core.last_waypoint_index - 5].location,
                 next_position=core.route[core.last_waypoint_index + 5].location)
-            # angle_between_waypoints_minus5 = abs_clip_normalise(angle_between_waypoints_minus5, math.pi)
+            angle_between_waypoints_minus5 = abs_clip_normalise(angle_between_waypoints_minus5, math.pi)
         else:
-            angle_between_waypoints_minus5 = math.pi
+            angle_between_waypoints_minus5 = 1
 
         if core.last_waypoint_index - 7 > 0:
             angle_between_waypoints_minus7 = calculate_angle_with_center_of_lane(
                 previous_position=core.route[core.last_waypoint_index].location,
                 current_position=core.route[core.last_waypoint_index - 7].location,
                 next_position=core.route[core.last_waypoint_index + 7].location)
-            # angle_between_waypoints_minus7 = abs_clip_normalise(angle_between_waypoints_minus7, math.pi)
+            angle_between_waypoints_minus7 = abs_clip_normalise(angle_between_waypoints_minus7, math.pi)
         else:
-            angle_between_waypoints_minus7 = math.pi
+            angle_between_waypoints_minus7 = 1
 
         if core.last_waypoint_index - 10 > 0:
             angle_between_waypoints_minus10 = calculate_angle_with_center_of_lane(
                 previous_position=core.route[core.last_waypoint_index].location,
                 current_position=core.route[core.last_waypoint_index - 10].location,
                 next_position=core.route[core.last_waypoint_index + 10].location)
-            # angle_between_waypoints_minus10 = abs_clip_normalise(angle_between_waypoints_minus10, math.pi)
+            angle_between_waypoints_minus10 = abs_clip_normalise(angle_between_waypoints_minus10, math.pi)
         else:
-            angle_between_waypoints_minus10 = math.pi
+            angle_between_waypoints_minus10 = 1
 
         if core.last_waypoint_index - 12 > 0:
             angle_between_waypoints_minus12 = calculate_angle_with_center_of_lane(
                 previous_position=core.route[core.last_waypoint_index].location,
                 current_position=core.route[core.last_waypoint_index - 12].location,
                 next_position=core.route[core.last_waypoint_index + 12].location)
-            # angle_between_waypoints_minus12 = abs_clip_normalise(angle_between_waypoints_minus12, math.pi)
+            angle_between_waypoints_minus12 = abs_clip_normalise(angle_between_waypoints_minus12, math.pi)
         else:
-            angle_between_waypoints_minus12 = math.pi
+            angle_between_waypoints_minus12 = 1
 
         angle_between_waypoints_5 = calculate_angle_with_center_of_lane(
             previous_position=core.route[core.last_waypoint_index+5].location,
             current_position=core.route[core.last_waypoint_index].location,
             next_position=core.route[core.last_waypoint_index + 10].location)
-        # angle_between_waypoints_5 = abs_clip_normalise(angle_between_waypoints_5,math.pi)
+        angle_between_waypoints_5 = abs_clip_normalise(angle_between_waypoints_5,math.pi)
 
         angle_between_waypoints_7 = calculate_angle_with_center_of_lane(
             previous_position=core.route[core.last_waypoint_index+7].location,
             current_position=core.route[core.last_waypoint_index].location,
             next_position=core.route[core.last_waypoint_index + 14].location)
-        # angle_between_waypoints_7 = abs_clip_normalise(angle_between_waypoints_7,math.pi)
+        angle_between_waypoints_7 = abs_clip_normalise(angle_between_waypoints_7,math.pi)
 
 
         if len(core.route) > core.last_waypoint_index + 20:
@@ -734,10 +734,10 @@ class SACExperimentBasic(BaseExperiment):
                 previous_position=core.route[core.last_waypoint_index + 10].location,
                 current_position=core.route[core.last_waypoint_index].location,
                 next_position=core.route[core.last_waypoint_index + 20].location)
-            # angle_between_waypoints_10 = abs_clip_normalise(angle_between_waypoints_10, math.pi)
+            angle_between_waypoints_10 = abs_clip_normalise(angle_between_waypoints_10, math.pi)
 
         else:
-            angle_between_waypoints_10 = math.pi
+            angle_between_waypoints_10 = 1
 
         if len(core.route) > core.last_waypoint_index + 24:
             angle_between_waypoints_12 = calculate_angle_with_center_of_lane(
@@ -747,7 +747,7 @@ class SACExperimentBasic(BaseExperiment):
             # angle_between_waypoints_12 = abs_clip_normalise(angle_between_waypoints_12, math.pi)
 
         else:
-            angle_between_waypoints_12 = math.pi
+            angle_between_waypoints_12 = 1
 
         # Angle to center of lane
 
