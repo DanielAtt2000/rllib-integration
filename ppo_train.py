770d010caa35ad657bc9e070c96bf1a468897d18
Daniel Attard <attarddan2017@gmail.com>
2023-10-21 12:20:13 +0200

    With aceleration changes lr: 0.000005

--- ppo_train.py
+++ ppo_train.py
@@ -161,7 +161,7 @@ def main():
 
 
     launch_tensorboard(logdir= path,
-                       host="localhost", port="6010")
+                       host="localhost", port="6011")
 
 
     specific_version = False
