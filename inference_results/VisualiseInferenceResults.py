c0dc71f1a5a39b01179802813d0012fd8ffcd8cb
Daniel Attard <attarddan2017@gmail.com>
2023-10-28 08:58:41 +0200

    DQN UPdated

--- inference_results/VisualiseInferenceResults.py
+++ inference_results/VisualiseInferenceResults.py
@@ -48,19 +48,26 @@ total_routes = 0
 truck_total_distance_to_center_of_lane_completed_routes = 0
 trailer_total_distance_to_center_of_lane_completed_routes = 0
 total_completed_routes_at_least_one = 0
+uncompleted_left_routes  =0
+uncompleted_right_routes = 0
+trailer_uncompleted_routes_dict  = {}
+truck_uncompleted_routes_dict  = {}
 
 def get_route_type(current_entry_idx, current_exit_idx):
+    lane = ''
     found = False
     ifroundabout20m = False
     ifupper_medium_roundabout = False
     iflower_medium_roundabout_all = False
-    ifdoubleRoundabout_roundabout_all = False
+    if50Roundabout_roundabout_all = False
+    if16Roundabout_roundabout_all = False
     for entry_easy in roundabout20m:
         entry_idx = entry_easy[0]
         if current_entry_idx == entry_idx:
             if current_exit_idx in entry_easy[1]:
                 found = True
                 ifroundabout20m = True
+                lane = entry_easy[2]
                 break
 
     if not found:
@@ -70,6 +77,7 @@ def get_route_type(current_entry_idx, current_exit_idx):
                 if current_exit_idx in entry_easy[1]:
                     found = True
                     ifupper_medium_roundabout = True
+                    lane = entry_easy[2]
                     break
     if not found:
         for entry_easy in (lower_medium_roundabout_easy+lower_medium_roundabout_difficult):
@@ -78,24 +86,33 @@ def get_route_type(current_entry_idx, current_exit_idx):
                 if current_exit_idx in entry_easy[1]:
                     found = True
                     iflower_medium_roundabout_all = True
+                    lane = entry_easy[2]
                     break
     if not found:
         for entry_easy in (spawn_points_2_lane_roundabout_small_easy+spawn_points_2_lane_roundabout_small_difficult):
             entry_idx = entry_easy[0]
             if current_entry_idx == entry_idx:
                 if current_exit_idx in entry_easy[1]:
+                    entryIndx50mRoundabout = [73,15,55,96,58,119,22,16,96,58]
                     found = True
-                    ifdoubleRoundabout_roundabout_all = True
+                    lane = entry_easy[2]
+                    if current_entry_idx in entryIndx50mRoundabout:
+                        if50Roundabout_roundabout_all = True
+                    else:
+                        if16Roundabout_roundabout_all = True
                     break
 
+
     if iflower_medium_roundabout_all:
-        return '32m'
+        return '32m',lane
     elif ifupper_medium_roundabout:
-        return '40m'
+        return '40m',lane
     elif ifroundabout20m:
-        return '20m'
-    elif ifdoubleRoundabout_roundabout_all:
-        return 'double'
+        return '20m',lane
+    elif if16Roundabout_roundabout_all:
+        return '16m',lane
+    elif if50Roundabout_roundabout_all:
+        return '50m',lane
     else:
         raise Exception('fuc')
 
@@ -133,8 +150,43 @@ for route in routes:
         x['timesteps'] = x['timesteps']/no_of_runs
         x['distance_to_center_of_lane'] = x['distance_to_center_of_lane']/no_of_runs
         x['trailer_distance_to_center_of_lane'] = x['trailer_distance_to_center_of_lane']/no_of_runs
-        x['roundabout'] = get_route_type(int(x['route'].split('|')[0]),int(x['route'].split('|')[1]))
+        x['roundabout'], lane = get_route_type(int(x['route'].split('|')[0]),int(x['route'].split('|')[1]))
 
+        if lane == 'left' and x['completed'] != 5:
+            for i in range(5-x['completed']):
+                uncompleted_left_routes+=1
+        if lane == 'right' and x['completed'] != 5:
+            for i in range(5-x['completed']):
+                uncompleted_right_routes+=1
+
+
+
+        if ((x['trailer_lidar_collision']> x['truck_lidar_collision']) or (x['trailer_lidar_collision'] != 0 and x['truck_lidar_collision'] == 0)) and x['completed'] != 5:
+            if trailer_uncompleted_routes_dict.get(x['roundabout']+lane) == None:
+                trailer_uncompleted_routes_dict[x['roundabout']+lane] = 1
+            else:
+                trailer_uncompleted_routes_dict[x['roundabout']+lane] += 1
+
+        elif ((x['truck_lidar_collision'] >x['trailer_lidar_collision']) or (x['truck_lidar_collision'] != 0 and x['trailer_lidar_collision'] == 0)) and x['completed'] != 5:
+            if truck_uncompleted_routes_dict.get(x['roundabout']+lane) == None:
+                truck_uncompleted_routes_dict[x['roundabout']+lane] = 1
+            else:
+                truck_uncompleted_routes_dict[x['roundabout']+lane] += 1
+        elif (x['truck_lidar_collision'] == x['trailer_lidar_collision']) and x['completed'] != 5:
+            for i in range(x['truck_lidar_collision']):
+                if truck_uncompleted_routes_dict.get(x['roundabout'] + lane) == None:
+                    truck_uncompleted_routes_dict[x['roundabout'] + lane] = 1
+                else:
+                    truck_uncompleted_routes_dict[x['roundabout'] + lane] += 1
+            for i in range(x['trailer_lidar_collision']):
+                if trailer_uncompleted_routes_dict.get(x['roundabout']+lane) == None:
+                    trailer_uncompleted_routes_dict[x['roundabout']+lane] = 1
+                else:
+                    trailer_uncompleted_routes_dict[x['roundabout']+lane] += 1
+        elif x['completed'] == 5:
+            pass
+        else:
+            raise Exception()
         # x['route'] = total_routes
         # if no_of_runs_successful < 15:
         print('----------------')
@@ -165,6 +217,11 @@ print(f'Mean TRUCK distance to center of lane for completed routes = {truck_tota
 print(f'Mean TRAILER distance to center of lane for completed routes = {trailer_total_distance_to_center_of_lane_completed_routes / total_completed_routes_at_least_one}')
 print(f'Total Routes = {total_routes}')
 
+print(f'Uncompleted left routes {uncompleted_left_routes} = {uncompleted_left_routes/(uncompleted_left_routes+uncompleted_right_routes)}')
+print(f'Uncompleted right routes {uncompleted_right_routes} = {uncompleted_right_routes/(uncompleted_left_routes+uncompleted_right_routes)}')
+
+print(f'TRUCK uncompleted_routes_dict {truck_uncompleted_routes_dict}')
+print(f'TRAILER uncompleted_routes_dict {trailer_uncompleted_routes_dict}')
 import seaborn as sns
 final_results = final_results.sort_values(by=['completed'])
 final_results = final_results
