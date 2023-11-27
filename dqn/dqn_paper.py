0f1353a5cba8ac207901ed33f692b14eb1de84a2
Daniel Attard <attarddan2017@gmail.com>
2023-11-27 16:39:15 +0100

    Paper Implementation

--- dqn/dqn_paper.py
+++ dqn/dqn_paper.py
@@ -46,7 +46,7 @@ class Vector:
 class DQNExperimentBasic(BaseExperiment):
     def __init__(self, config={}):
         super().__init__(config)  # Creates a self.config with the experiment configuration
-        self.acceleration_pid = PID(Kp=0.2,Ki=0.0,Kd=0.0,setpoint=1.9,sample_time=None,output_limits=(-1,1))
+        self.acceleration_pid = PID(Kp=0.2,Ki=0.0,Kd=0.0,setpoint=2.5,sample_time=None,output_limits=(-1,1))
         self.frame_stack = self.config["others"]["framestack"]
         self.max_time_idle = self.config["others"]["max_time_idle"]
         self.max_time_episode = self.config["others"]["max_time_episode"]
