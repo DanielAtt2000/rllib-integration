ed19e00ccd8d3873a47b446ca0b7f8f2d98b0b62
Daniel Attard <attarddan2017@gmail.com>
2023-11-28 17:31:33 +0100

    Paper Implementation

--- DataAnalysis/generate_reward_per_episode_graph.py
+++ DataAnalysis/generate_reward_per_episode_graph.py
@@ -1,13 +1,16 @@
 import pickle
-
+import statistics
 import numpy as np
 from matplotlib import pyplot as plt
+import scipy.stats as st
 
 def open_pickle(filename):
     with open(filename + '.pickle', 'rb') as handle:
         return pickle.load(handle)
 def run(filenames=[],label_names = [],window_size=[]):
     all_reward = []
+    reward_uppers = []
+    reward_lowers = []
     all_done = []
 
     plt.rcParams.update({'font.size': 13})
@@ -23,41 +26,83 @@ def run(filenames=[],label_names = [],window_size=[]):
             window_size.append(window_reward)
     plt.figure(figsize=(9, 5))
 
-    for filename,window in zip(filenames,window_size):
+    for filename,window,label in zip(filenames,window_size,label_names):
         done_array = open_pickle(f'done/{filename}_done')
         rewards = open_pickle(f'rewards/{filename}_total_reward')
 
+        print(label)
+        print(f'Standard deviation {statistics.stdev(rewards)}')
+        print(st.t.interval(alpha=0.95, df=len(rewards) - 1,
+                      loc=np.mean(rewards),
+                      scale=st.sem(rewards)))
+        print(st.norm.interval(alpha=0.95,
+                 loc=np.mean(rewards),
+                 scale=st.sem(rewards)) )
+
+
         average = []
+        reward_upper = []
+        reward_lower = []
         stopping_value = min(len(done_array),max_value)
         last_index_done = 0
 
         for ind in range(stopping_value,0+window-1,-1):
             x = np.mean(rewards[ind - window:ind])
             last_index_done = ind
+            a = statistics.stdev(rewards[ind - window:ind])
+            reward_upper.append(x+a)
+            reward_lower.append(x-a)
             average.append(x)
 
         last_average = average[-1]
         for ind in range(last_index_done, 0,-1):
             x = np.mean(rewards[:ind])
-
+            if len(rewards[:ind]) < 2:
+                print('here')
+                reward_upper.append(0)
+                reward_lower.append(0)
+            else:
+                a = statistics.stdev(rewards[:ind])
+                reward_upper.append(x + a)
+                reward_lower.append(x - a)
             average.append(x)
         # for i in range(window):
         #     average.append(0)
         average.reverse()
+        reward_upper.reverse()
+        reward_lower.reverse()
+        reward_uppers.append(reward_upper)
+        reward_lowers.append(reward_lower)
         all_reward.append(average)
     # for ind in range(len(all_episodes_easy_sum) - window + 1):
     #     average_easy_y.append(np.mean(all_episodes_easy_sum[ind:ind + window]))
     colours = ['b','g','r','c','m','y','k','w']
-    for label,reward, colour in zip(label_names,all_reward,colours):
+    longest = 0
+    for reward in all_reward:
+        if len(reward) > longest:
+            longest = len(reward)
+    for label,reward, colour, reward_upper, reward_lower in zip(label_names,all_reward,colours, reward_uppers, reward_lowers):
+        std_dev = statistics.stdev(reward)
+        reward_plus = [r + std_dev for r in reward]
+        reward_minus =[r - std_dev for r in reward]
         plt.plot(reward,label=label,color= colour)
+        if colour == 'b':
+            c2 = (0.5,0.5,1)
+        elif colour == 'r':
+            c2 = (1,0.5,0.5)
+        elif colour == 'g':
+            c2= (0.5,1,0.5)
+        elif colour == 'c':
+            c2 = (0.5,1,1)
+        plt.fill_between(range(len(reward)),reward_lower,reward_upper,color=c2,alpha=0.7)
     # plt.plot(average_easy_y,label='average_easy_y ')
 
     # plt.plot(all_point_difficult_rewards,label='all_point_difficult_rewards')
     # plt.plot(all_line_difficult_rewards,label='all_line_difficult_rewards')
     if not different_window_sizes:
-        plt.ylabel(f'Episode reward averaged over \n a {window_reward} episode sliding window')
+        plt.ylabel(f'Episode return averaged over \n a {window_reward} episode sliding window')
     else:
-        plt.ylabel(f'Episode reward averaged over \n different episode sliding window sizes')
+        plt.ylabel(f'Episode return averaged over \n different episode sliding window sizes')
 
     plt.xlabel('Number of episodes')
 
@@ -98,10 +143,10 @@ def run(filenames=[],label_names = [],window_size=[]):
 # run(['2c5a756566f','2c5a756566f'], label_names= ['2/4 - 200 window','2/4 - 1500 window'], window_size=[200,1500])
 
 # Run for differnt weight vectors
-# run(['257465bf42a','169760e0cd9','1f96591647f','770d010caa3'], label_names= ['0.5/400','1.0/400','1.5/400','2.0/400'])
+run(['257465bf42a','169760e0cd9','1f96591647f','770d010caa3'], label_names= ['0.5/400','1.0/400','1.5/400','2.0/400'])
 
 # Run for different state spaces
-run(['fe92e6d36e8','a1fc8624646','169760e0cd9'], label_names= ['Angles','Radii','Angles and Radii'])
+# run(['fe92e6d36e8','a1fc8624646','169760e0cd9'], label_names= ['Angles','Radii','Angles and Radii'])
 
 
 # Constant vs Variable velocity action space
